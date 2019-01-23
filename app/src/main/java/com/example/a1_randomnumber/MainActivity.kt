79456cb16e0c9ed85dbf656280fff1591b32f5f1
SambathPich <sambathstorage@gmail.com>
2019-01-23 18:25:12 -0500

    "Show Current SeekBar Value"

--- app/src/main/java/com/example/a1_randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/a1_randomnumber/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.a1_randomnumber
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -16,21 +14,49 @@ class MainActivity : AppCompatActivity() {
         val seekBar = findViewById<SeekBar>(R.id.seekBar)
         val resultTextView = findViewById<TextView>(R.id.resultTextView)
         val startButton = findViewById<Button>(R.id.startButton)
+        val currentSeekBarMaxValue = findViewById<TextView>(R.id.currentMaxValueTextView)
+
+
+        //Show Current Seekbar Value
+        currentSeekBarMaxValue.text = "Current Max Value: " + seekBar.progress
 
 
         startButton.setOnClickListener {
 
             if (seekBar.progress > 0) {
-                val randomNumber = Random.nextInt(seekBar.progress) + 1
-                resultTextView.text = randomNumber.toString()
+                val random = Random.nextInt(seekBar.progress) + 1
+                resultTextView.text = random.toString()
             } else {
                 resultTextView.text = "0"
             }
 
+
         }
+
+
+        seekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                currentSeekBarMaxValue.text = "Current Max Value: " + seekBar.progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // When touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                //When touch is stopped.
+            }
+        })
+
 
 
 
 
     }
+
+
+
+
 }
+
